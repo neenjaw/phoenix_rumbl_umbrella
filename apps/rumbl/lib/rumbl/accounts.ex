40bd3055ec1a8070765a3ddd6d826171902ec809
Tim Austin <tim@neenjaw.com>
2020-09-24 12:13:45 -0600

    rumbl added

---
+++ apps/rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,69 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  import Ecto.Query
+
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def list_users() do
+    Repo.all(User)
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from u in User, where: u.id in ^ids)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  # This is for user creation methods that do not require passwords
+  # eg. oauth
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  # This is for creating users with passwords
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def authenticate_by_username_and_pw(username, given_pw) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(given_pw, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
+end
